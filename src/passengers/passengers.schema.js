7dd6fa607b86681a493909c92fa51acc32a4f10f
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-05 15:16:35 -0600

    validations-schema-created

--- src/passengers/passengers.schema.js
+++ src/passengers/passengers.schema.js
@@ -1,1 +1,46 @@
+/* validaciones con zod */
+
+import { json } from 'sequelize'
 import z from 'zod'
+
+export const passengerSchema = z.object({
+  /* validame que sea string y tenga un minimo de 8 caracters y maximo de 10 caracteres */
+  nroPassport: z.string().min(8).max(10),
+  name: z.string().min(2).max(99),
+  surname: z.string().min(2).max(100),
+  /* errores personalizados dependiendo del errror */
+  birthdate: z.string({
+    invalid_type_error: 'Birthdate must have a correct format',
+    required_error: 'Birthdate is required'
+  }),
+  /* validaciones con enum */
+  genre: z.enum(['male', 'female', 'prefer not to say']),
+  email: z.string().email(),
+  phone: z.string().min(5).max(25)
+})
+
+export function validatePassenger(data) {
+  const result = passengerSchema.safeParse(data)
+  const { hasErrror, errorMessage, data } = extractValidationData(result)
+  return {
+    hasErrror,
+    errorMessage,
+    data
+  }
+}
+
+export const extractValidationData = (resultValidation) => {
+  let errorMessage
+  let data
+  const hasErrror = !resultValidation.success
+
+  if (hasErrror) errorMessage = json.parse(resultValidation.error.message)
+
+  if (!hasErrror) data = resultValidation.data
+
+  return {
+    hasErrror,
+    errorMessage,
+    data
+  }
+}
