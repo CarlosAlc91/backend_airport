f4675c99b9b68c7e8d13a9c771aeb1cb41537d60
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-05 02:02:18 -0600

    fixing-routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express"
 import { router as passengerRouter } from '../passengers/passenger.route.js'
+import { router as citiesRouter } from "../cities/cities.route.js"
 
 
 export const router = Router()
 
 router.use('/passengers', passengerRouter)
+router.use('/cities', citiesRouter)
