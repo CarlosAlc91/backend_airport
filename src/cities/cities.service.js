67b00ba4477aec9cdb3a8e5c3c4fdd36e8a8969e
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-08 04:05:36 -0600

    updating-methods-into-cities.service

--- src/cities/cities.service.js
+++ src/cities/cities.service.js
@@ -2,23 +2,39 @@ import City from './cities.model.js'
 
 
 export class CityService {
-
+  //retorname todas las ciudades donde su status sea true
   async findAllCities() {
-
+    return await City.findAll({
+      where: {
+        status: true
+      }
+    })
   }
 
   async createCity(data) {
     return await City.create(data)
   }
 
-  async findOneCity() {
-
+  //retorna una ciudad en donde su id sea igual al id y su status sea true
+  async findOneCity(id) {
+    return await City.findOne({
+      where: {
+        id,
+        status: true
+      }
+    })
   }
-  async updateCity() {
 
+  //se recibe la ciudad y la data a actualiza
+  async updateCity(city, data) {
+    return await city.update(data)
   }
-  async deleteCity() {
 
+  //solo se recibe la ciudad a elimnar porque se hace una eliminacion de manera logica - actualizando unicamente el status a false
+  async deleteCity(city) {
+    return await city.update({
+      status: false
+    })
   }
 
 }
