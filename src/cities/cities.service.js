3de9fb69fcc1f8e817e2d2a655e0b31c256ef53d
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-08 03:43:22 -0600

    adding-methods-into-cities.service

--- src/cities/cities.service.js
+++ src/cities/cities.service.js
@@ -2,7 +2,23 @@ import City from './cities.model.js'
 
 
 export class CityService {
+
+  async findAllCities() {
+
+  }
+
   async createCity(data) {
     return await City.create(data)
   }
+
+  async findOneCity() {
+
+  }
+  async updateCity() {
+
+  }
+  async deleteCity() {
+
+  }
+
 }
