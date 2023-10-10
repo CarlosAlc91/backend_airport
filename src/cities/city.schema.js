94c55ce5b2191535a41a6dae59b0a6d4e89c4543
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-10 00:06:01 -0600

    creating-update-validation-in-city.schema

--- src/cities/city.schema.js
+++ src/cities/city.schema.js
@@ -9,7 +9,7 @@ import z from 'zod'
 import { extractValidationData } from '../common/utils/extractErrorData.js'
 
 //2. crear la constante del schema
-//aqui vna las validaciones de los campos del modal 
+//aqui van las validaciones de los campos del modal 
 const citySchema = z.object({
   name: z.string().min(3).max(60),
   country: z.string().min(3).max(60),
@@ -36,3 +36,25 @@ export const validateCity = (data) => {
     cityData
   }
 }
+
+//4. validacion para la actualizacion de la ciudad
+export const validatePartialCity = (data) => {
+
+  //1. creacion de variable result para hacer la validacion
+  const result = citySchema.partial().safeParse(data)
+
+  //2. obtener los resulatados de la validacion
+  const {
+    hasErrror,
+    errorMessage,
+    data: dataCity
+  } = extractValidationData()
+
+  //3. retorno de los resultados
+  return {
+    hasErrror,
+    errorMessage,
+    data: dataCity
+  }
+}
+
