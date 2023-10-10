65b36f86e256881d5e4796a05f2df087f19436b7
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-10 17:59:21 -0600

    adding-validation-middleware-on-app.js

--- src/app.js
+++ src/app.js
@@ -6,5 +6,13 @@ const app = express()
 app.use(express.json())
 app.use('/api/v1', router)
 
+/* validacion sino se encuntran rutas de routes.js siga buscando */
+app.all('*', (req, res, next) => {
+  return res.status(404).json(({
+    status: 'error',
+    message: `cannot find route ${req.originalUrl}`
+  }))
+})
+
 export default app
 
