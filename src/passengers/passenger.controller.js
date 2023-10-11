b59fee5bdca7bfa3cf0bbbd586c937e8d3a59e75
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-11 00:13:37 -0600

    catch-errors-function-created-invoking-function-in-CRUD-on-passenger.controller

--- src/passengers/passenger.controller.js
+++ src/passengers/passenger.controller.js
@@ -4,134 +4,120 @@ import { PassengerService } from './passengers.service.js'
 /* instancia de la clase PassengerService */
 const passengerService = new PassengerService()
 
-//1. get all passengers
-export const findAllPassengers = async (req, res) => {
-
-
-  try {
-    const passengers = await passengerService.findAllPassengers(req.body)
-    return res.json(passengers)
-  } catch (error) {
-    return res.status(500).json(error)
+/* fncion para la captura de errores */
+//1. creacion de funcion
+const catchAsync = fn => {
+  //las promesas pueden ser consumidas con el .then
+  //2. utilizar el async y await como parametros del callback catchAsync de la funcion que vamos a utilizar
+  //3. retorna el req, res y next en la fn 
+  return (req, res, next) => {
+    //con esta linea ya se estan manejando los errores
+    fn(req, res, next).catch(next)
   }
 }
+
+
+//1. get all passengers
+export const findAllPassengers = catchAsync(async (req, res, next) => {
+  const passengers = await passengerService.findAllPassengers()
+  return res.json(passengers)
+})
 
 //2. create a passenger
-export const createPassesnger = async (req, res) => {
+export const createPassesnger = catchAsync(async (req, res, next) => {
   /* const passanger = req.body */
   /* aqui se pueden crear los datos, pasajero */
-  try {
+  const { hasErrror, errorMessage, passengerData } = validatePassenger(req.body)
 
-    const { hasErrror, errorMessage, passengerData } = validatePassenger(req.body)
-
-    if (hasErrror) {
-      return res.status(422).json({
-        status: 'error',
-        message: errorMessage
-      })
-    }
-
-
-    const passanger = await passengerService.createPassenger(passengerData)
-    /* siempre a las respuestas se les pone un return, cuando se tienen 2 res en el mismo ciclo la app va a fallar porque no se pueden enviar 2 al mismo tiemopo */
-    return res.status(201).json(passanger)
-
-  } catch (error) {
-
-    /*  if (error.parent.code === '22001') {
-       return res.status(400).json({
-         status: 'error',
-         message: 'Information received is too long'
-       })
-     } */
-
-    return res.status(500).json(error)
+  if (hasErrror) {
+    return res.status(422).json({
+      status: 'error',
+      message: errorMessage
+    })
   }
-}
+
+  const passanger = await passengerService.createPassenger(passengerData)
+  /* siempre a las respuestas se les pone un return, cuando se tienen 2 res en el mismo ciclo la app va a fallar porque no se pueden enviar 2 al mismo tiemopo */
+  return res.status(201).json(passanger)
+
+})
 
 //3. find a passenger by id
-export const findPassengerById = async (req, res) => {
-  try {
-    const { id } = req.params
+export const findPassengerById = catchAsync(async (req, res, next) => {
 
-    const passanger = await passengerService.findOnePassenger(id)
+  const { id } = req.params
 
-    if (!passanger) {
-      return res.status(404).json({
-        status: "error",
-        message: `passenger with ${id} not found ⚠️`
-      })
-    }
+  const passanger = await passengerService.findOnePassenger(id)
 
-    res.json(passanger)
-  } catch (error) {
-    return res.status(500).json(error)
+  if (!passanger) {
+    return res.status(404).json({
+      status: "error",
+      message: `passenger with ${id} not found ⚠️`
+    })
   }
-}
+
+  res.json(passanger)
+
+})
 
 //4. update passenger
-export const updatePassenger = async (req, res) => {
+export const updatePassenger = catchAsync(async (req, res, next) => {
 
-  try {
-    //TODO: PUNTO A
-    /* es mas optimo primero hacer la validaacion de los campos y despues hacer la busqueda del passenger*/
-    const { hasErrror, errorMessage, passengerData } = validatePartialPassenger(req.body)
 
-    if (hasErrror) {
-      return res.status(422).json({
-        status: 'error',
-        message: errorMessage
-      })
-    }
-    //1. obtener el id del passenger
-    const { id } = req.params
+  //TODO: PUNTO A
+  /* es mas optimo primero hacer la validaacion de los campos y despues hacer la busqueda del passenger*/
+  const { hasErrror, errorMessage, passengerData } = validatePartialPassenger(req.body)
 
-    //2. buscar el pasajero que se va a actualizar
-    const passenger = await passengerService.findOnePassenger(id)
-
-    //TODO: PUNTO B
-    //3. validar si el pasajero existe
-    if (!passenger) {
-      return res.status(404).json({
-        status: 'error',
-        message: `passenger with id ${id} not found ⚠️`
-      })
-    }
-    //TODO: PUNTO C
-    //4. si existe, se actualiza el passenger
-    const passengerUpdated = await passengerService.updatePassenger(passenger, req.body)
-
-    //5. retorno de passenger validado
-    return res.json(passengerUpdated)
-
-  } catch (error) {
-    return res.status(500).json(error)
+  if (hasErrror) {
+    return res.status(422).json({
+      status: 'error',
+      message: errorMessage
+    })
   }
-}
+  //1. obtener el id del passenger
+  const { id } = req.params
+
+  //2. buscar el pasajero que se va a actualizar
+  const passenger = await passengerService.findOnePassenger(id)
+
+  //TODO: PUNTO B
+  //3. validar si el pasajero existe
+  if (!passenger) {
+    return res.status(404).json({
+      status: 'error',
+      message: `passenger with id ${id} not found ⚠️`
+    })
+  }
+  //TODO: PUNTO C
+  //4. si existe, se actualiza el passenger
+  const passengerUpdated = await passengerService.updatePassenger(passenger, req.body)
+
+  //5. retorno de passenger validado
+  return res.json(passengerUpdated)
+
+})
 
 //5. delete passenger
-export const deletePassenger = async (req, res) => {
+export const deletePassenger = catchAsync(async (req, res, next) => {
 
 
-  try {
-    const { id } = req.params
 
-    /* get passsenger by id */
-    const passenger = await passengerService.findOnePassenger(id)
+  const { id } = req.params
 
-    /* validate if passsenger exists */
-    if (!passenger) {
-      return res.status(404).json({
-        status: 'error',
-        message: `passenger with id ${id} not found ⚠️`
-      })
-    }
+  /* get passsenger by id */
+  const passenger = await passengerService.findOnePassenger(id)
 
-    /* regeso del passenger eliminado */
-    await passengerService.deletePassenger(passenger)
-
-    return res.status(204).json(null)
-  } catch (error) {
-    return res.status(500).json(error)
+  /* validate if passsenger exists */
+  if (!passenger) {
+    return res.status(404).json({
+      status: 'error',
+      message: `passenger with id ${id} not found ⚠️`
+    })
   }
-}
+
+  /* regeso del passenger eliminado */
+  await passengerService.deletePassenger(passenger)
+
+  return res.status(204).json(null)
+
+})
