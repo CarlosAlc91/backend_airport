dfb9c0a1c95204344969ecf8bdfabe181ef12a4b
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-11 02:29:09 -0600

    model-and-file-created-in-error.model.js

---
+++ src/errors/error.model.js
@@ -0,0 +1,29 @@
+/* creacion de modelo */
+//1. importat datatypes
+import { DataTypes } from "sequelize"
+import sequelize from "../config/database/database"
+
+//2. creacion de modelo
+const Error = sequelize.define('error', {
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    type: DataTypes.INTEGER
+  },
+  status: {
+    type: DataTypes.STRING(10),
+    allowNull: true
+  },
+  message: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  stack: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  }
+})
+
+//3. exportat
+export default Error
