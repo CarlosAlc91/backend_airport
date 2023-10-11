7cacd189c572d6f8434f2336261cb8c506abd559
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-10 18:16:32 -0600

    adding-errors-middleware-on-app.js

--- src/app.js
+++ src/app.js
@@ -14,5 +14,20 @@ app.all('*', (req, res, next) => {
   }))
 })
 
+//basurero = aqui queda la basura (errores)
+app.use((err, req, res, next) => {
+  //en el err va a venir una propiedad que se llama statusCode en donde solo van a llegar errores de la gama del 400, si es undefined me vas a colocasr 500
+  err.statusCode = err.statusCode || 500
+
+  //aqui solo van a llegar status de error y si viene undefined le vas a colocar failed
+  err.status = err.status || 'failed'
+
+  res.status(err.statusCode).json({
+    //aqui puede venir o error o failed
+    status: err.status,
+    message: err.message
+  })
+})
+
 export default app
 
