f0f2c0f707ea146383db32ca4cdcef5121ca966c
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-11 01:34:21 -0600

    import-and-calling-globlalErrorHandler-in-app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { router } from "./routes/routes.js"
 import { AppError } from "./errors/appError.js"
 import morgan from "morgan"
 import { envs } from "./config/environments/environments.js"
+import { globalErrorHandler } from "./errors/error.controller.js"
 
 const app = express()
 
@@ -48,19 +49,7 @@ app.all('*', (req, res, next) => {
 })
 
 //basurero - controlador de errores = aqui queda la basura (errores)
-app.use((err, req, res, next) => {
-  //en el err va a venir una propiedad que se llama statusCode en donde solo van a llegar errores de la gama del 400, si es undefined me vas a colocasr 500
-  err.statusCode = err.statusCode || 500
-
-  //aqui solo van a llegar status de error y si viene undefined le vas a colocar failed
-  err.status = err.status || 'failed'
-
-  res.status(err.statusCode).json({
-    //aqui puede venir o error o failed
-    status: err.status,
-    message: err.message
-  })
-})
+app.use(globalErrorHandler)
 
 export default app
 
