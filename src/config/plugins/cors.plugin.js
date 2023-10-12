39849edb3b4084490e64ec6e84243f438ad8f34c
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-12 01:13:26 -0600

    importing-enableCors-in-app.js

--- src/config/plugins/cors.plugin.js
+++ src/config/plugins/cors.plugin.js
@@ -1,6 +1,6 @@
 //1. importacion de cors
 import cors from 'cors'
-import app from '../../app'
+
 
 //2. exportacion de funcion la cual va a recibir app y acceptedOrigins
 export const enableCors = (app, acceptedOrigins) => {
