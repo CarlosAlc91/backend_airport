ed02d7c04d49b6d45172d12b68b3ee929845a8b6
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-12 01:20:55 -0600

    adding-objext-in-cors.plugin

--- src/config/plugins/cors.plugin.js
+++ src/config/plugins/cors.plugin.js
@@ -5,5 +5,13 @@ import cors from 'cors'
 //2. exportacion de funcion la cual va a recibir app y acceptedOrigins
 export const enableCors = (app, acceptedOrigins) => {
   //3.
-  app.use(cors())
+  app.use(cors({
+    //4. despues de crear accepted_origins en app.js
+    origin: (origin, callback) => {
+      //5. si acceptedOrigins incluye origin voy a retornar una funcion callback que va a devolver un null y true
+      if (acceptedOrigins.include(origin)) {
+        return callback(null, true)
+      }
+    }
+  }))
 }
