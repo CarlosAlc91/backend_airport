9322655f0814c649db08385e417ba1cb0b944d06
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-12 00:46:17 -0600

    importinf-and-calling-error.model-in-error.controller.js

--- src/errors/error.controller.js
+++ src/errors/error.controller.js
@@ -47,6 +47,7 @@ const sendErrorProd = async (err, res) => {
   }
 }
 
+/* aqui llegan todos los errores */
 export const globalErrorHandler = (err, req, res, next) => {
   //en el err va a venir una propiedad que se llama statusCode en donde solo van a llegar errores de la gama del 400, si es undefined me vas a colocasr 500
   err.statusCode = err.statusCode || 500
