49035cd69e79e175c1968c844bf0765dcd8e2c3c
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-19 03:41:13 -0600

    controller-created-in-auth.controller.js

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,10 +1,28 @@
 //1. import catchAsync from index.js
 import { catchAsync } from '../errors/index.js'
+import { validateRegister } from './user.schema.js'
 
-//2. export login and register
-export const login = catchAsync(async (req, res, next) => {
-
-})
+//2. export login and register:
+//2.1 register
 export const register = catchAsync(async (req, res, next) => {
+  //1. refactor from validateRegister
+  const {
+    hasError,
+    errorMessage,
+    userData
+  } = validateRegister(req.body)
+
+  //2. conditional for errors
+  if (hasError) {
+    return res.status(422).json({
+      status: 'error',
+      message: errorMessage
+    })
+  }
+
+  //3. create user into auth.service.js
+})
+
+export const login = catchAsync(async (req, res, next) => {
 
 })
